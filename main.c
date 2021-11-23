31c7a618adcffcd27bdb619aa8d7956fbff55d9f
Ruslan <89871727+Lofranlof@users.noreply.github.com>
2021-11-23 21:33:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
     float var;
     char string[30];
     scanf("%[^\n]s", string);
-    scanf("%f", &var);
+    scanf("%f", var);//ошибка, не хватает &
     printf("\n%s %f", string, var);
     return 0;
 }
